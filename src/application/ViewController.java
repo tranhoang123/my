bca4650124f10f70c9487806f6f7e8ae3ddf0758
tranhoang123 <hoang97sonic@gmail.com>
2018-11-26 00:44:59 +0700

    third

--- src/application/ViewController.java
+++ src/application/ViewController.java
@@ -7,9 +7,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 //import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -17,6 +20,8 @@ import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.stage.DirectoryChooser;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 public class ViewController  implements Initializable  {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -45,6 +50,8 @@ public class ViewController  implements Initializable  {
 	private Button repeat;
 	@FXML
 	private Button Folder;
+	@FXML
+	private ScrollPane scrollPane;
 	@FXML
 	private void handlerButtonAction(ActionEvent event) {
 		FileChooser fileChooser = new FileChooser();
@@ -80,14 +87,42 @@ public class ViewController  implements Initializable  {
 		 	DirectoryChooser directoryChooser = new DirectoryChooser();
 
 		 	 File dir = directoryChooser.showDialog(null);
+		 	 VBox re = new VBox();
+		 	 scrollPane.setContent(re);
+		 	 re.setSpacing(10);
              if (dir != null) {
                  //System.out.println(dir); 
             	 File[] listFile = dir.listFiles();
-            	 listSong.setText(null);
-            	 System.out.println(listFile[0].getName());
+            	 // listSong.setText(null);
+            	 //System.out.println(listFile[0].getName());
+            	 Media media = new Media(listFile[0].toURI().toASCIIString()); // lấy path của file đầu tiên làm mặc định
+   				 mediaPlayer = new MediaPlayer(media);
+   				 mediaShow.setMediaPlayer(mediaPlayer);
             	 for(File file: listFile) {
-            		 listSong.appendText(file.getName()+"\n");
+//            		 listSong.appendText(file.getName()+"\n");
+            		 Button button = new Button(); //tạo nút mới
+            		 button.setText(file.getName()); // đặt tên nút
+            		 button.setId(file.getPath()); // đặt ID cho nút, lấy id bằng path luôn
+            		 button.setOnAction(click->{   // tạo một event trong nút
+            			
+//            			for(File file1 : listFile) {
+//            				mediaPlayer = new MediaPlayer(new Media(file1.toURI().toASCIIString()));
+            				mediaPlayer.stop(); // dừng cái đang phát hiện tại
+//            				System.out.println("dm");
+//            			}
+            			mediaPlayer = new MediaPlayer(new Media(file.toURI().toASCIIString())); //đổi thành media mới
+         				mediaPlayer.play();
+//         				for(File file1: listFile) {
+//                    		 if(file1.toURI().toString() != file.toURI().toString()) {
+//                    			 mediaPlayer.stop();
+//                    		 }
+//                    	 }
+            		 });
+
+            		 //Label lb =new Label(file.getName());
+            		re.getChildren().add(button); // tạo nút xong xuôi thì thêm vào VBOX -> quay lại dòng 90
             	 }
+            	
              } else {
                  System.out.println("nothing");
              }
@@ -96,7 +131,12 @@ public class ViewController  implements Initializable  {
 	 private void showSong(ActionEvent event) {
 		 System.out.println("you click me");
 		 //boolean vis = listSong.
-		 listSong.setVisible(true);
+		 scrollPane.setVisible(true);
 	 }
+//	 private void playMusic(ActionEvent event) {
+//		 	Media media = new Media(this.filePath);
+//			mediaPlayer = new MediaPlayer(media);
+//			mediaShow.setMediaPlayer(mediaPlayer);
+//	 }
 }
 
